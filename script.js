8278627aa5f897ca1463bce8ce65809daceb6d18
ELMudyr <163483089+ELMudyr@users.noreply.github.com>
2024-05-02 23:18:33 +0100

    Update script.js added collapsable nav menu

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function nav() {
   const collapseButton = document.getElementById("collapse");
   const collapseButton2 = document.getElementById("collapse2");
   const greyMenu = document.getElementById("greymenu");
-
+  const navLink = document.getElementById("Features");
+  const navLink2 = document.getElementById("Company");
+  const hoverMenu = document.getElementById("hovermenu");
+  const hoverMenu2 = document.getElementById("hovermenu2");
   // Toggle submenu visibility
   submenuItems.forEach((item) => {
     item.addEventListener("click", function (event) {
@@ -20,21 +23,44 @@ function nav() {
     });
   });
 
+  //Toggle Features Submenu Desktop
+
+  navLink.addEventListener("click", () => {
+    hoverMenu.classList.toggle("open");
+  });
+
+  //Toggle Company Submenu Desktop
+
+  navLink2.addEventListener("click", () => {
+    console.log("clicked");
+    hoverMenu2.classList.toggle("open");
+  });
+
+  //Open Side Menu Mobile
+
   burger.addEventListener("click", () => {
     menu.classList.toggle("open");
     greyMenu.classList.toggle("open");
   });
 
+  //Close Side Menu Mobile
+
   closeIcon.addEventListener("click", () => {
     menu.classList.remove("open");
     greyMenu.classList.remove("open");
   });
+
+  //Toggle Features Submenu Mobile
+
   collapseButton.addEventListener("click", () => {
     const submenus = document.querySelectorAll(".submenu");
     submenus.forEach((submenu) => {
       submenu.classList.toggle("open");
     });
   });
+
+  //Toggle Company Submenu Mobile
+
   collapseButton2.addEventListener("click", () => {
     const submenus = document.querySelectorAll(".submenu2");
     submenus.forEach((submenu) => {
